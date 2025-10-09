244f4d6b5c89a08b769a4494a5cd0b9cdb3ff077
Nayara Ogioni <nayara.ogioni4587@gmail.com>
2025-10-09 18:27:23 +0100

    first settings

--- src/arg_check.c
+++ src/arg_check.c
@@ -6,7 +6,29 @@
 /*   By: nayara <nayara@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 18:04:34 by nayara            #+#    #+#             */
-/*   Updated: 2025/10/09 18:04:46 by nayara           ###   ########.fr       */
+/*   Updated: 2025/10/09 18:18:25 by nayara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "cube.h"
+
+static int	check_ext(const char *str)
+{
+	char	*ext;
+
+	ext = ".cub";
+	if (ft_strncmp(str + ft_strlen(str) - 4, ext, 4) != 0)
+		return (0);
+	return (1);
+}
+
+int	arg_check(int ac, char **av)
+{
+	if (ac < 2)
+		return (ft_printf("Wrong number of arguments"), 0);
+	if (ac > 2)
+		return (ft_printf("Wrong number of arguments"), 0);
+	if (!check_ext(av[1]))
+		return (ft_printf("Error"), 0);
+	return (1);
+}
