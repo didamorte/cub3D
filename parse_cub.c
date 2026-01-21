a61b59c7876dd3c18d57fb68564baaae5ae7f791
Nayara Ogioni <nayara.ogioni4587@gmail.com>
2026-01-21 10:25:36 +0000

    error return alterations

--- parse_cub.c
+++ parse_cub.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nayara <nayara@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/01/06 19:09:59 by nayara            #+#    #+#             */
-/*   Updated: 2026/01/20 16:20:50 by nayara           ###   ########.fr       */
+/*   Created: 2026/01/20 18:58:03 by nayara            #+#    #+#             */
+/*   Updated: 2026/01/20 20:26:15 by nayara           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,42 @@
 int	parse_identifiers(t_game *g, int fd)
 {
 	char	*line;
-	int		found_elements;
+	int		res;
+	int		found_identifiers;
 
-	found_elements = 0;
-	while (found_elements < 6)
+	found_identifiers = 0;
+	line = get_next_line(fd);
+	while (line)
 	{
-		line = get_next_line(fd);
-		if (!line)
-			break ;
 		if (is_empty_line(line))
 		{
 			free(line);
+			line = get_next_line(fd);
 			continue ;
 		}
-		if (!process_line(g, line))
+		res = process_line(g, line);
+		if (res == 1)
+			found_identifiers++;
+		else if (res == 0)
 		{
 			free(line);
-			clear_gnl(fd);
-			free_textures_paths(g);
 			return (printf("Error\nInvalid configuration element\n"), 0);
 		}
-		found_elements++;
+		else if (res == -1)
+		{
+			if (found_identifiers < 6)
+			{
+				free(line);
+				return (printf("Error\nMissing identifiers before map\n"), 0);
+			}
+			g->map_line = line;
+			return (1);
+		}
 		free(line);
+		line = get_next_line(fd);
 	}
-	return (found_elements == 6);
+	return (found_identifiers == 6);
 }
-
 
 int	check_map_chars(t_game *g)
 {
@@ -97,19 +107,18 @@ int	is_map_closed(t_game *g)
 	return (1);
 }
 
-char	*read_map_to_string(int fd)
+char	*read_map_to_string(t_game *g, int fd)
 {
 	char	*line;
 	char	*tmp;
 	char	*full_str;
 
-	full_str = ft_strdup("");
+	if (!g->map_line)
+		return (NULL);
+	full_str = ft_strdup(g->map_line);
+	free(g->map_line);
+	g->map_line = NULL;
 	line = get_next_line(fd);
-	while (line && is_empty_line(line))
-	{
-		free(line);
-		line = get_next_line(fd);
-	}
 	while (line)
 	{
 		tmp = full_str;
@@ -118,8 +127,6 @@ char	*read_map_to_string(int fd)
 		free(line);
 		line = get_next_line(fd);
 	}
-	if (ft_strlen(full_str) == 0)
-		return (free(full_str), NULL);
 	return (full_str);
 }
 
@@ -136,14 +143,18 @@ int	parse_cub_file(t_game *g, char *file_path)
 		close(fd);
 		return (0);
 	}
-	map_buffer = read_map_to_string(fd);
+	map_buffer = read_map_to_string(g, fd);
 	if (!map_buffer)
-		return (printf("Error\nMap is missing\n"));
+	{
+		close(fd);
+		return (printf("Error\nMap is missing\n"), 0);
+	}
 	g->map = ft_split(map_buffer, '\n');
 	free(map_buffer);
 	close(fd);
 	if (!check_map_chars(g))
-		return (printf("Error\nInvalid characters or multiple players\n"), 0);
+		return (printf("Error\nInvalid characters, \
+multiple players or NO player\n"), 0);
 	if (!is_map_closed(g))
 		return (printf("Error\nMap is not closed by walls\n"), 0);
 	return (1);
